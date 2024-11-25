5ad4cd15483ae42ed59bee6c4c684c88f35b32ee
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-11-25 20:54:24 -0300

    Ajustando bottom navigation

--- src/routes/bottom.routes.jsx
+++ src/routes/bottom.routes.jsx
@@ -1,34 +1,59 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../pages/home';
-import Orders from '../pages/orders';
-import Profile from '../pages/profile';
-import Admin from '../pages/admin/index';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../pages/home";
+import Orders from "../pages/orders";
+import Profile from "../pages/profile";
+import Admin from "../pages/admin/index";
+import Icon from "react-native-vector-icons/MaterialIcons"; // Importa a biblioteca de ícones
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomRoutes() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name="Inicio"
-                component={Home}
-            />
-
-            <Tab.Screen
-                name="Pedidos"
-                component={Orders}
-            />
-
-            <Tab.Screen
-                name="Perfil"
-                component={Profile}
-            />
-
-            <Tab.Screen
-                name="Admin"
-                component={Admin}
-            />
-
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false, // Remove o header no topo
+        tabBarStyle: {
+          backgroundColor: "#fff", // Define o estilo da barra inferior
+          height: 50, // Ajusta a altura da barra
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size || 24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pedidos"
+        component={Orders}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="shopping-cart" color={color} size={size || 24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="person" color={color} size={size || 24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Admin"
+        component={Admin}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="settings" color={color} size={size || 24} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
