48ffe3344cae9224e870b0f55c74c8d2e59d621c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-12-28 23:26:51 -0300

    Iniciando implementação de notificações

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -9,6 +9,7 @@ import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
 import PasswordInput from '../../components/passwordInput';
 import CustomInput from '../../components/customInput';
+import * as Notifications from "expo-notifications";  // Importar expo-notifications
 
 export default function Login() {
     const navigation = useNavigation();
@@ -18,6 +19,35 @@ export default function Login() {
     const [isLoading, setIsLoading] = useState(false);
     const userStorge = new UserStorage();
 
+    // Função para obter o token de push e enviar para a API
+    const registerForPushNotificationsAsync = async () => {
+        try {
+            const { status } = await Notifications.requestPermissionsAsync();
+            if (status !== "granted") {
+                alert("Falha ao obter permissão para notificações!");
+                return;
+            }
+            const token = (await Notifications.getExpoPushTokenAsync({
+                projectId: 'dc3085fd-ddd4-4403-9841-1cd7e16ba489',
+              })).data;
+            console.log("Expo Push Token:", token);
+
+            // Enviar o token para a API (exemplo de requisição)
+            await fetch('https://sua-api.com/api/save-push-token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    token: token,
+                    userId: userStorge.getUserData().id, // Enviar o ID do usuário armazenado
+                }),
+            });
+        } catch (error) {
+            console.error("Erro ao obter o token de push:", error);
+        }
+    };
+
     const handleLogin = async () => {
         setIsLoading(true);
         try {
@@ -26,9 +56,12 @@ export default function Login() {
                 if (data instanceof LoginResponse) {
                     navigation.reset({
                         index: 0,
-                        routes: [{ name: 'BottomRoutes' }],
+                        routes: [{ name: 'BottomRoutes' }], // Navegação após login
                     });
                     userStorge.saveUserData({ id: data.id, name: data.name, role: data.role, token: data.token });
+
+                    // Após o login, registre o token de push
+                    await registerForPushNotificationsAsync();  // Registra e envia o token
                 } else {
                     Alert.alert('Erro', data);
                 }
@@ -40,7 +73,7 @@ export default function Login() {
         } finally {
             setIsLoading(false);
         }
-    };    
+    };
 
     return (
         <View style={styles.container}>
